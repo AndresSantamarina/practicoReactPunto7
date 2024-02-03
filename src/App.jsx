d89899a1c62a1de099ac73e3e8ac6d051cfd52c8
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-03 06:34:55 -0300

    borre un import que daba error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "./components/Footer";
 import EmpleadoList from "./components/EmpleadoList";
 
 function App() {
